28fd60be499af02e7479d7cc2664f13874d36740
Alexander <alexbrianzhang@gmail.com>
2019-10-19 22:55:09 -0400

    Add gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # notoriOS
 > To all the teachers that told me I'd never amount to nothin
+
+A non-preemptive operating system for node.
--- notoriOS/notoriOS.cydsn/codegentemp/RTC_INT.c
+++ notoriOS/notoriOS.cydsn/codegentemp/RTC_INT.c
@@ -55,7 +55,6 @@ static void RTC_EverySecondHandler(void)
 {
     /*  Place your every second handler code here. */
     /* `#START EVERY_SECOND_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_SECOND);
     /* `#END` */
     
     #ifdef RTC_EVERY_SECOND_HANDLER_CALLBACK
@@ -82,7 +81,6 @@ static void RTC_EveryMinuteHandler(void)
 {
     /*  Place your every minute handler code here. */
     /* `#START EVERY_MINUTE_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_MINUTE);
     /* `#END` */
 
     #ifdef RTC_EVERY_MINUTE_HANDLER_CALLBACK
@@ -109,7 +107,6 @@ static void RTC_EveryHourHandler(void)
 {
     /*  Place your every hour handler code here. */
     /* `#START EVERY_HOUR_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_HOUR);
     /* `#END` */
     
     #ifdef RTC_EVERY_HOUR_HANDLER_CALLBACK
@@ -136,7 +133,6 @@ static void RTC_EveryDayHandler(void)
 {
     /*  Place your everyday handler code here. */
     /* `#START EVERY_DAY_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_DAY);
     /* `#END` */
     
     #ifdef RTC_EVERY_DAY_HANDLER_CALLBACK
@@ -163,7 +159,6 @@ static void RTC_EveryWeekHandler(void)
 {
     /*  Place your every week handler code here. */
     /* `#START EVERY_WEEK_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_WEEK);
     /* `#END` */
 
     #ifdef RTC_EVERY_WEEK_HANDLER_CALLBACK
@@ -190,7 +185,6 @@ static void RTC_EveryMonthHandler(void)
 {
     /*  Place your every month handler code here. */
     /* `#START EVERY_MONTH_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_MONTH);
     /* `#END` */
     
     #ifdef RTC_EVERY_MONTH_HANDLER_CALLBACK
@@ -217,7 +211,6 @@ static void RTC_EveryYearHandler(void)
 {
     /*  Place your every year handler code here. */
     /* `#START EVERY_YEAR_HANDLER_CODE` */
-    AyoItsTime(ALARM_TYPE_YEAR);
     /* `#END` */
 
     #ifdef RTC_EVERY_YEAR_HANDLER_CALLBACK
