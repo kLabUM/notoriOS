c61afd7bf5da567e48057cc487df9d48003b39c8
Jon <93671340+Jniej@users.noreply.github.com>
2022-04-04 10:44:31 -0400

    New TEMP code
    It's the carbon copy of Travis's code. I went line by line and I still have a type def error even though the boards work in the same way.

--- notoriOS.cydsn/temperature.c
+++ notoriOS.cydsn/temperature.c
@@ -44,9 +44,179 @@ void TEMP_Stop() {
     TEMP_ISR_Stop();
 }
 
-TEMP_t TEMP_read(){
+TEMP_sensor_t Temp_read(){
+
     TEMP_clear();
-    TEMP_t tf;
+    TEMP_sensor_t values;
+    
+    TEMP_Talk();
+    //char excerpt[80]; // for debugging
+    DO_UART_PutString("C,0\r"); // turn off automated sampling
+    
+    // turn "*OK" response off
+    CyDelay(1000u);
+    TEMP_UART_PutString("*OK,0\r");
+    CyDelay(2000u);
+    /*
+    for (uint i = 0; i < 80; i++){
+        excerpt[i] = do_received_string[i];  
+        if (i>78u){
+            printNotif(NOTIF_TYPE_EVENT,"asdf");
+        }
+    }
+    */
+    TEMP_UART_ClearRxBuffer();
+    TEMP_clear(); // get rid of anything we've received so far
+    // should just be the startup messages 
+
+    // take 11 readings
+    for (int i = 0; i < 11; i++){
+        TEMP_UART_PutString("R\r"); // take one reading
+        CyDelay(1200u); // docs say readings take one second
+    }
+
+
+    char *token;    
+    token = strtok(temperature_received_string, "\r");
+
+    int i = 0;
+    float32 reading;
+    while( token != NULL ) {
+        reading = atof(token);
+        values.all_TEMP_readings[i] = reading;
+        token = strtok(NULL, "\r"); // This "NULL" says to continue where you left off last time.    
+        i++;
+    }
+
+    // readings array should have eleven nice floats now
+    // TODO: check this and throw an error if not
+   
+    values.TEMP_reading = float_find_median(values.all_TEMP_readings, 11);
+    
+    // turn "*OK" response back on (this is default and useful for debugging)
+    TEMP_UART_PutString("*OK,1\r");
+    CyDelay(1000u);
+    TEMP_Stop();
+    
+
+    
+    
+    return values;
+}
+
+test_t TEMP_sensor_test(){
+    TEMP_clear();
+    char excerpt[300]; // for debugging
+    // only call this in the lab for initial setup (calibration to air)
+    TEMP_UART_PutString("Factory\r"); // take one reading
+    CyDelay(2000u); // docs say readings take one second
+    // DO_cal() should be commented out in the main repo and never called in the field
+    //DO_cal();
+    
+    TEMP_sensor_t tresults = TEMP_Read();
+
+    test_t test;
+    
+        test.status = 0;
+        // test reason  
+        snprintf(test.reason,sizeof(test.reason), 
+        "DO readings invalid\r\nDetails:\r\nmedian reading:%f \r\nall readings (11): %f : %f : %f : %f : %f : %f : %f : %f : %f : %f : %f",
+        tresults.TEMP_reading,
+        tresults.all_TEMP_readings[0], tresults.all_TEMP_readings[1], tresults.all_TEMP_readings[2], 
+        tresults.all_TEMP_readings[3], tresults.all_TEMP_readings[4], tresults.all_TEMP_readings[5], 
+        tresults.all_TEMP_readings[6], tresults.all_TEMP_readings[7], tresults.all_TEMP_readings[8], 
+        tresults.all_TEMP_readings[9], tresults.all_TEMP_readings[10]
+        );
+        return test;
+
+    // for now, getting something besides zeros is valid
+    test.status = 1; // set test status to zero
+    snprintf(test.test_name,sizeof(test.test_name),"TEST_DO_SENSOR");
+   
+    
+    for (uint i = 0; i < 300; i++){
+        excerpt[i] = temperature_received_string[i];  
+    }
+    
+
+    return test;
+    
+}
+
+void Temp_Debug() {
+    
+    TEMP_clear();
+    TEMP_Talk();
+    char excerpt[300]; // for debugging
+    // take 11 readings
+    for (int i = 0; i < 11; i++){
+        TEMP_UART_PutString("R\r"); // take one reading
+        CyDelay(1200u); // docs say readings take one second
+    }
+    TEMP_UART_PutString("*OK,1\r");
+    CyDelay(1000u);
+    TEMP_UART_PutString("C,1\r");
+    CyDelay(5000u);
+    for (uint i = 0; i < 300; i++){
+        excerpt[i] = temperature_received_string[i];  
+    }
+    printNotif(NOTIF_TYPE_EVENT, "Temperature Sensor is Initialized");
+    
+    TEMP_Stop();
+}
+
+
+
+// code for finding medians duplicated from level_sensor because we need floats not longs
+
+// function to calculate the median of the array, after array is sorted
+float32 float_find_median(float32 array[] , uint8 n){
+    float32 median=0;
+    fsort(array,n);
+    
+    // if number of elements are even
+    if(n%2 == 0)
+        // median is the average of the  two middle sorted numbers
+        median = (array[(n-1)/2] + array[n/2])/2.0;
+    // if number of elements are odd
+    else
+        // median is the the middle sorted number 
+        median = array[n/2];
+    
+    return median;
+}
+
+// function to sort the elements in the level readings array
+void fsort(float32 a[],uint8 n) { 
+   uint8 i,j;
+
+   for(i = 0;i < n-1;i++) {
+      for(j = 0;j < n-i-1;j++) {
+         if(a[j] > a[j+1])
+            fswap(&a[j],&a[j+1]); // swap the address of a[j] and a[j+1]
+      }
+   }
+}
+
+// function to swap elements in the level readings array in order to sort them
+void fswap(float32 *p,float32 *q) {
+   float32 t;
+   
+   t=*p; 
+   *p=*q; 
+   *q=t;
+}
+
+
+
+
+
+
+
+/*
+T_sensor_t TEMP_read(){
+    TEMP_clear();
+    T_sensor_t tf;
     
     TEMP_Talk();
     TEMP_UART_PutString("C,0\r"); // turn off automated sampling
@@ -77,14 +247,12 @@ TEMP_t TEMP_read(){
         i++;
     }
 
-    // readings array should have eleven nice floats now
-    // TODO: check this and throw an error if not
-   
+    tf.TEMP_reading = float_find_median(tf.all_TEMP_readings, 11);
     
     // turn "*OK" response back on (this is default and useful for debugging)
-    DO_UART_PutString("*OK,1\r");
+    TEMP_UART_PutString("*OK,1\r");
     CyDelay(1000u);
-    DO_stop_talking();
+    TEMP_Stop();
     return tf;
 }
 
@@ -92,7 +260,7 @@ test_t TEMP_test(){
     TEMP_UART_PutString("Factory\r"); // take one reading
     CyDelay(2000u); // docs say readings take one second
     
-    TEMP_t values = TEMP_read();
+    T_sensor_t values = TEMP_read();
 
    test_t test;
     
@@ -138,3 +306,44 @@ void Temp_Debug() {
     TEMP_Stop();
 }
 
+// code for finding medians duplicated from level_sensor because we need floats not longs
+
+// function to calculate the median of the array, after array is sorted
+float32 float_find_median(float32 array[] , uint8 n){
+    float32 median=0;
+    fsort(array,n);
+    
+    // if number of elements are even
+    if(n%2 == 0)
+        // median is the average of the  two middle sorted numbers
+        median = (array[(n-1)/2] + array[n/2])/2.0;
+    // if number of elements are odd
+    else
+        // median is the the middle sorted number 
+        median = array[n/2];
+    
+    return median;
+}
+
+// function to sort the elements in the level readings array
+void fsort(float32 a[],uint8 n) { 
+   uint8 i,j;
+
+   for(i = 0;i < n-1;i++) {
+      for(j = 0;j < n-i-1;j++) {
+         if(a[j] > a[j+1])
+            fswap(&a[j],&a[j+1]); // swap the address of a[j] and a[j+1]
+      }
+   }
+}
+
+// function to swap elements in the level readings array in order to sort them
+void fswap(float32 *p,float32 *q) {
+   float32 t;
+   
+   t=*p; 
+   *p=*q; 
+   *q=t;
+}
+
+*/
--- notoriOS.cydsn/temperature.h
+++ notoriOS.cydsn/temperature.h
@@ -6,11 +6,42 @@
 typedef struct { 
     float32 all_TEMP_readings[TEMP_N_READINGS];
     float32 TEMP_reading;
-} TEMP_t;
+} TEMP_sensor_t;
+
+test_t TEMP_sensor_test();
+
+TEMP_sensor_t TEMP_read();
+
+
+float32 float_find_median(float32 array[] , uint8 n);
+
+// function to sort the elements in the level readings array
+void fsort(float32 a[],uint8 n);
+
+// function to swap elements in the level readings array in order to sort them
+void fswap(float32 *p,float32 *q);
+
+// calibrate sensor to atmospheric oxygen levels
+// see "single piont calibration" in datasheet
+// this should be done once, before deploying, as calibration is robust to power cycling
+uint8 TEMP_Cal();
+
+// prepare lines of communication
+void TEMP_Talk();
+
+
+// pull everything low to stop power leaks
+void TEMP_Stop();
+
+/*
+#define TEMP_N_READINGS 11
+
+typedef struct { 
+    float32 all_TEMP_readings[TEMP_N_READINGS];
+    float32 TEMP_reading;
+} T_sensor_t;
 
 test_t TEMP_test();
-
-TEMP_t TEMP_read();
 
 // establish a debugging and reading struct
 void TEMP_Debug();
@@ -24,3 +55,4 @@ void TEMP_Talk();
 
 // Temp shut down 
 void TEMP_Stop();
+*/
